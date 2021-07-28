ab234791a276e758d3259763f98449566d640824
Anton Sizikov <sizikov.anton@gmail.com>
2021-07-28 22:27:41 +0200

    infra: add keyvault and move settings there

--- infra/CloudEng.InvoiceBuilder.Infrastructure/InvoiceBuilderStack.cs
+++ infra/CloudEng.InvoiceBuilder.Infrastructure/InvoiceBuilderStack.cs
@@ -1,9 +1,15 @@
+using CloudEng.InvoiceBuilder.Infrastructure.Security;
 using Pulumi;
+using Pulumi.AzureNative.Authorization;
+using Pulumi.AzureNative.Insights;
 using Pulumi.AzureNative.Resources;
 using Pulumi.AzureNative.Storage;
-using Pulumi.AzureNative.Storage.Inputs;
 using Pulumi.AzureNative.Web;
 using Pulumi.AzureNative.Web.Inputs;
+using Kind = Pulumi.AzureNative.Storage.Kind;
+using ManagedServiceIdentityType = Pulumi.AzureNative.Web.ManagedServiceIdentityType;
+using SkuArgs = Pulumi.AzureNative.Storage.Inputs.SkuArgs;
+using SkuName = Pulumi.AzureNative.Storage.SkuName;
 
 namespace CloudEng.InvoiceBuilder.Infrastructure {
   // ReSharper disable once ClassNeverInstantiated.Global
@@ -18,15 +24,14 @@ namespace CloudEng.InvoiceBuilder.Infrastructure {
       var fileStorageAccount = CreateFileStorageAccount("files-storage", "sacloudenginvoicefiles", resourceGroup);
       var functionStorageAccount = CreateFileStorageAccount("function-storage", "sacloudenginvoicefunc", resourceGroup);
 
-      var appServicePlan = new AppServicePlan("asp-invoice-builder-func", new AppServicePlanArgs {
+      var appServicePlan = CreateAppServicePlan(resourceGroup);
+      var clientConfig = GetClientConfig.InvokeAsync().Result;
+
+      var appInsights = new Component("invoice-builder-app-insights", new ComponentArgs {
+        ApplicationType = ApplicationType.Web,
+        Kind = "web",
         ResourceGroupName = resourceGroup.Name,
-        Location = resourceGroup.Location,
-        Kind = "FunctionApp",
-        Reserved = true,
-        Sku = new SkuDescriptionArgs {
-          Tier = "Dynamic",
-          Name = "Y1"
-        }
+        Location = resourceGroup.Location
       });
 
       var builderFunctionApp = new WebApp("invoice-builder-app", new WebAppArgs {
@@ -34,6 +39,9 @@ namespace CloudEng.InvoiceBuilder.Infrastructure {
         Kind = "FunctionApp",
         ServerFarmId = appServicePlan.Id,
         Reserved = true,
+        Identity = new ManagedServiceIdentityArgs {
+          Type = ManagedServiceIdentityType.SystemAssigned,
+        },
         SiteConfig = new SiteConfigArgs {
           AppSettings = new[] {
             new NameValuePairArgs {
@@ -47,13 +55,43 @@ namespace CloudEng.InvoiceBuilder.Infrastructure {
             new NameValuePairArgs {
               Name = "AzureWebJobsStorage",
               Value = GetConnectionString(resourceGroup.Name, functionStorageAccount.Name)
-            }
+            },
+            new NameValuePairArgs {
+              Name = "APPLICATIONINSIGHTS_CONNECTION_STRING",
+              Value = Output.Format($"InstrumentationKey={appInsights.InstrumentationKey}"),
+            },
+            //app config
+            new NameValuePairArgs {
+              Name = "TogglClient.ApiKey",
+              Value = "@Microsoft.KeyVault(SecretUri=https://invoice-builder-vault.vault.azure.net/secrets/TogglClient-ApiKey)"
+            },
+            new NameValuePairArgs {
+              Name = "TogglClient.UserAgent",
+              Value = "@Microsoft.KeyVault(SecretUri=https://invoice-builder-vault.vault.azure.net/secrets/TogglClient-UserAgent)"
+            },
+            new NameValuePairArgs {
+              Name = "TogglClient.WorkspaceId",
+              Value = "@Microsoft.KeyVault(SecretUri=https://invoice-builder-vault.vault.azure.net/secrets/TogglClient-WorkspaceId)"
+            },
           }
         }
       });
-
-
+      var functionPrincipalId = builderFunctionApp.Identity.Apply(response => response.PrincipalId );
+      var keyVault = new KeyVault(resourceGroup, clientConfig, functionPrincipalId);
       ResourceGroupName = resourceGroup.Name;
+    }
+
+    private static AppServicePlan CreateAppServicePlan(ResourceGroup resourceGroup) {
+      return new AppServicePlan("asp-invoice-builder-func", new AppServicePlanArgs {
+        ResourceGroupName = resourceGroup.Name,
+        Location = resourceGroup.Location,
+        Kind = "FunctionApp",
+        Reserved = true,
+        Sku = new SkuDescriptionArgs {
+          Tier = "Dynamic",
+          Name = "Y1"
+        }
+      });
     }
 
     private static StorageAccount CreateFileStorageAccount(string filesStorageName, string filesAccountName, ResourceGroup resourceGroup) {
---
+++ infra/CloudEng.InvoiceBuilder.Infrastructure/Security/KeyVault.cs
@@ -0,0 +1,41 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.Authorization;
+using Pulumi.AzureNative.KeyVault;
+using Pulumi.AzureNative.KeyVault.Inputs;
+using Pulumi.AzureNative.Resources;
+
+namespace CloudEng.InvoiceBuilder.Infrastructure.Security {
+  public class KeyVault {
+    public KeyVault(ResourceGroup resourceGroup, GetClientConfigResult clientConfig, Output<string> functionPrincipalId) {
+      var keyVault = new Vault("invoice-builder-vault", new VaultArgs {
+        Location = resourceGroup.Location,
+        Properties = new VaultPropertiesArgs {
+          AccessPolicies = new[] {
+            new AccessPolicyEntryArgs {
+              TenantId = clientConfig.TenantId,
+              ObjectId = clientConfig.ObjectId,
+              Permissions = new PermissionsArgs {
+                Secrets = new InputList<Union<string, SecretPermissions>> {"get", "set", "list"}
+              }
+            },
+            new AccessPolicyEntryArgs {
+              TenantId = clientConfig.TenantId,
+              ObjectId = functionPrincipalId,
+              Permissions = new PermissionsArgs {
+                Secrets = new InputList<Union<string, SecretPermissions>> {"get"}
+              }
+            }
+          },
+          Sku = new SkuArgs {
+            Family = SkuFamily.A,
+            Name = SkuName.Standard
+          },
+          TenantId = clientConfig.TenantId,
+        },
+
+        ResourceGroupName = resourceGroup.Name,
+        VaultName = "invoice-builder-vault",
+      });
+    }
+  }
+}
